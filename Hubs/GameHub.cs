e7fb6a06c7daa57eba07938b76b413b1b107434f
srquike <srquike@outlook.com>
2023-08-14 22:19:17 -0600

    Initial commmit

---
+++ Hubs/GameHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TicTacToeSignalR.API.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task SelectSquare(string gameId, int selectedSquare, int move)
+        {
+            try
+            {
+                await Clients.Group(gameId).SendAsync("SelectedSquare", selectedSquare, move);
+                Console.WriteLine($"Se selecciono el {selectedSquare} cuadrado en el movimiento {move}.");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task Join(string gameId, string player)
+        {
+            try
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, gameId);
+                await Clients.Group(gameId).SendAsync("join", player);
+                Console.WriteLine($"El jugador {player} se unio");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Models/MoveModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TicTacToeSignalR.API.Models
+{
+    public class MoveModel
+    {
+        public string? Player { get; set; }
+        public int Position { get; set; }
+    }
+}
